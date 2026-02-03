aef29f03be214748480b694de08802aacf15f7da
Bruno de Assis Ferreira <brunodassis30@gmail.com>
2026-02-03 13:59:19 -0300

    Refactor
    - Refatorando funções dentro do service de reserva para remover redundâncias e adicionar o pagamento.

--- src/reserva/reserva.module.ts
+++ src/reserva/reserva.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
 import { ReservaController } from './reserva.controller';
 import { ReservaService } from './reserva.service';
+import { PagamentoConsumer } from './pagamento.controller';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { SessaoModule } from 'src/sessao/sessao.module';
+import { SeedModule } from 'src/seed/seed.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Pagamento } from './entities/pagamento.entity';
 
 @Module({
-    imports: [],
-    controllers: [ReservaController],
+    imports: [
+        TypeOrmModule.forFeature([Pagamento]),
+        ClientsModule.register([
+            {
+                name: 'PEDIDO_SERVICE',
+                transport: Transport.RMQ,
+                options: {
+                    urls: ['amqp://guest:guest@rabbitmq:5672'],
+                    queue: 'pedidos_queue',
+                    queueOptions: { durable: true },
+                },
+            },
+        ]), SessaoModule, SeedModule],
+    controllers: [ReservaController, PagamentoConsumer],
     providers: [ReservaService],
 })
 export class ReservaModule { }
--- src/reserva/reserva.service.ts
+++ src/reserva/reserva.service.ts
@@ -1,30 +1,118 @@
-import { CACHE_MANAGER } from '@nestjs/cache-manager';
-import { Inject, Injectable } from '@nestjs/common';
 import type { Cache } from 'cache-manager';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { BadRequestException, Inject, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+
+import { SessaoService } from 'src/sessao/sessao.service';
+import { ProcessandoPagamentoDto } from './dto/pagamento-payload.dto';
+import { ReservaAssentoDto } from './dto/reserva-payload.dto';
+import { SeedService } from 'src/seed/seed.service';
+import { Repository } from 'typeorm';
+import { Pagamento, StatusPagamento } from './entities/pagamento.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+
 
 @Injectable()
 export class ReservaService {
-    constructor(@Inject(CACHE_MANAGER) private cacheManager: Cache) { }
+    constructor(
+        @Inject(CACHE_MANAGER) private cacheManager: Cache,
+        @Inject('PEDIDO_SERVICE') private readonly client: ClientProxy,
+        private sessaoService: SessaoService,
+        private seedService: SeedService,
+        @InjectRepository(Pagamento) private pagamentoRepo: Repository<Pagamento>,
+    ) { }
 
-    async solicitarReserva(assentoId: string, userId: string) {
-        const cacheKey = `seat_lock:${assentoId}`;
+    async solicitarReserva(payload: ReservaAssentoDto) {
+        const { assento_id, usuario_id } = payload;
+        try {
+            await this.validaAssentoEUsuario(assento_id, usuario_id);
 
-        const existingReservation = await this.cacheManager.get(cacheKey);
+            const cacheKey = `seat_lock:${assento_id}`;
+            const existing = await this.cacheManager.get(cacheKey);
 
-        if (existingReservation) {
-            return {
-                success: false,
-                message: 'Este assento já está reservado.',
-            };
+            if (existing) {
+                throw new BadRequestException('Assento ocupado.');
+            }
+
+            await this.cacheManager.set(cacheKey, usuario_id, 30000);
+
+            return { success: true, message: 'Assento reservado por 30s.' };
+        } catch (error) {
+            return { success: false, message: error.message };
         }
-
-        await this.cacheManager.set(cacheKey, userId, 100000);
-
-        return {
-            success: true,
-            message: `Assento ${assentoId} reservado para o usuário ${userId}.`,
-            expiresIn: '30s',
-        };
     }
 
+    async confirmarIntencaoCompra(payload: ReservaAssentoDto) {
+        const { assento_id, usuario_id } = payload;
+        try {
+            this.validaAssentoEUsuario(assento_id, usuario_id);
+
+            const cacheKey = `seat_lock:${assento_id}`;
+            const reservadoPor = await this.cacheManager.get(cacheKey);
+
+            if (!reservadoPor || reservadoPor !== usuario_id) {
+                throw new BadRequestException('Reserva expirou.');
+            }
+
+            await this.cacheManager.set(cacheKey, usuario_id, 600000);
+
+            return { success: true, message: 'Pagamento em processamento...' };
+        } catch (error) {
+            return { success: false, message: error.message };
+        }
+    }
+
+    async iniciandoPagamento(payload: ProcessandoPagamentoDto) {
+
+        const { assento_id, usuario_id } = payload;
+
+        await this.sessaoService.validaAssento(assento_id);
+        await this.validaReserva(assento_id, usuario_id);
+
+        this.client.emit('processar_pagamento', {
+            ...payload,
+            dataSolicitacao: new Date()
+        });
+
+        return { success: true, message: 'Pagamento processado com sucesso.' };
+    }
+
+    async cancelarIntencaoCompra(assento_id: string) {
+        const cacheKey = `seat_lock:${assento_id}`;
+        await this.cacheManager.del(cacheKey);
+        return { success: true, message: 'Reserva cancelada.' };
+    }
+
+    async validaAssentoEUsuario(assento_id: string, usuario_id: string) {
+        await this.sessaoService.validaAssento(assento_id);
+        await this.seedService.validarUsuario(usuario_id);
+    }
+
+    async validaReserva(assento_id: string, usuario_id: string) {
+        const cacheKey = `seat_lock:${assento_id}`;
+        const reservadoPor = await this.cacheManager.get(cacheKey);
+
+        if (!reservadoPor || reservadoPor !== usuario_id) {
+            throw new BadRequestException('Reserva expirou.');
+        }
+    }
+
+    async processarPagamentoExterno(dados: ProcessandoPagamentoDto): Promise<boolean> {
+        try {
+            const novoPagamento = this.pagamentoRepo.create({
+                assento_id: dados.assento_id,
+                usuario_id: dados.usuario_id,
+                cartao_final: dados.card_number.slice(-4),
+                titular_nome: dados.card_holder_name,
+                valor: dados.valor_da_compra,
+                metodo_pagamento: dados.tipo_pagamento,
+                status: StatusPagamento.APROVADO,
+
+            });
+            await this.pagamentoRepo.save(novoPagamento);
+            return true
+        } catch (error) {
+            throw new InternalServerErrorException('Erro ao processar pagamento externo.');
+        }
+    }
 }
